657a09b559a0d338d9ff758715c5fa179fa614ec
Yuliia Boiko <littlehappinies@gmail.com>
2022-09-16 16:16:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,30 +2,32 @@ const argv = require("yargs").argv;
 const contacts = require("./contacts");
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
-  switch (action) {
-    case "list":
-        const contactsList = await contacts.listContacts();
-        console.table(contactsList);
-        break;
+    switch (action) {
+        case "list":
+            const contactsList = await contacts.listContacts();
+            console.table(contactsList);
+            break;
 
-    case "get":
-        const oneContact = await contacts.getContactById(id);
-        console.log(oneContact);
-        break;
+        case "get":
+            const oneContact = await contacts.getContactById(id);
+            console.log(oneContact);
+            break;
 
-    case "add":
-        const newContact = await contacts.addContact({ name, email, phone });
-        console.log(newContact);
-        break;
+        case "add":
+            const newContact = await contacts.addContact({ name, email, phone });
+            console.log(newContact);
+            break;
 
-    case "remove":
-        const contactRemove = await contacts.removeContact(id);
-        console.log(contactRemove);
-        break;
+        case "remove":
+            const contactRemove = await contacts.removeContact(id);
+            console.log(contactRemove);
+            break;
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
-  }
-}
+        default:
+            console.warn("\x1B[31m Unknown action type!");
+    }
+};
 
-invokeAction(argv);
+(async () => {
+    await invokeAction(argv)
+})();
